a85a1767a49390fb98c14959872ab0f07e6868f8
saulius <sjsaulius@gmail.com>
2023-07-10 17:20:04 +0300

    add reset function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const p1Button = document.querySelector("#p1Button");
 const p2Button = document.querySelector("#p2Button");
 const p1Score = document.querySelector("#p1Score");
 const p2Score = document.querySelector("#p2Score");
+const reset = document.querySelector("#reset");
 
 let p1Display = 0;
 let p2Display = 0;
@@ -27,4 +28,12 @@ p2Button.addEventListener("click", function (e) {
 
     p2Score.textContent = p2Display;
   }
+});
+
+reset.addEventListener("click", function (e) {
+  isGameOver = false;
+  p1Display = 0;
+  p2Display = 0;
+  p1Score.textContent = 0;
+  p2Score.textContent = 0;
 });
